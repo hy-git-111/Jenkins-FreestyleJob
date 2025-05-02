9279b25901ff39419b23de1b32ba5cbf3fd320d4
hy-git-111 <hyeyoung.k111@gmail.com>
2025-05-02 15:53:40 +0900

    실행 권한 코드 추가

--- 250502_jenkins-pipelinejob/build.sh
+++ 250502_jenkins-pipelinejob/build.sh
@@ -1,1 +1,7 @@
 echo 'Building the application...'
+
+# 실행 권한 부여
+cd 250502_jenkins-pipelinejob
+git update-index –chmod=+x build.sh
+git commit -m “fix: add execute permission to build.sh”
+git push origin main
