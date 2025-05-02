d1bb6fba9da75ac6f056b46d93daaf03af7e321f
hy-git-111 <hyeyoung.k111@gmail.com>
2025-05-02 15:56:14 +0900

    잘못된 코드 삭제

--- 250502_jenkins-pipelinejob/build.sh
+++ 250502_jenkins-pipelinejob/build.sh
@@ -1,7 +1,1 @@
 echo 'Building the application...'
-
-# 실행 권한 부여
-cd 250502_jenkins-pipelinejob
-git update-index –chmod=+x build.sh
-git commit -m “fix: add execute permission to build.sh”
-git push origin main
